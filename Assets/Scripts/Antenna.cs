4b8e82e8927b5a381660e3a92c113f9832e92b45
Wridzer Kamphuis <wridzer12@gmail.com>
2022-01-29 22:33:34 +0100

    antenna code?

---
+++ Assets/Scripts/Antenna.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Antenna : MonoBehaviour
+{
+    [SerializeField] private GameObject[] signalPoints;
+    private GameObject bestSignal;
+
+    private AudioSource audioSource;
+    [SerializeField] private AudioClip distortion;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        bestSignal = signalPoints[Random.Range(0, signalPoints.Length - 1)];
+    }
+
+    public void NewSignal()
+    {
+        bestSignal = signalPoints[Random.Range(0, signalPoints.Length - 1)];
+    }
+
+    public void GotAntenna(GameObject orb)
+    {
+        if (orb != bestSignal)
+        {
+            DistortAudio();
+        }
+    }
+
+    private void DistortAudio()
+    {
+        audioSource.PlayOneShot(distortion);
+    }
+}
